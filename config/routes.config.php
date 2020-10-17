030edbdf4aabf9a3c5c0293add1b84fc0768151c
Marco Carvalho <marcocarvalho.web@gmail.com>
2020-10-17 12:38:51 -0300

    Configurations

---
+++ config/routes.config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'defaults' => [
+        'controller' => 'Home',
+        'action' => 'index'
+    ]
+];
--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,18 @@
 namespace FastStart;
 
 class Application {
+    private $configurations = [];
+    private $route = null;
+
+    public function getConfiguration($section) {
+        $path_def = $this->getFrameworkPath().'/config/'.$section.'.config.php';
+        $conf_def = (file_exists($path_def)) ? include($path_def) : [];
+        $path_app = $this->getApplicationPath().'/config/'.$section.'.config.php';
+        $conf_app = (file_exists($path_app)) ? include($path_app) : [];
+        $this->configurations[$section] = array_merge_recursive($conf_def, $conf_app);
+        return $this->configurations[$section];
+    }
+
     public function getFrameworkPath() {
         return realpath(__DIR__.'/../');
     }
